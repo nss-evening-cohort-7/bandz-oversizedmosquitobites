8843bd5359fc9767ff2f6f817cc86cdc66523fe0
Mehret Kulala <merutaye@gmail.com>
2018-03-16 09:05:16 -0500

    merchpage ready for review

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
 function writeToDom(domString, domId){
   var myDiv = document.getElementById(domId);
-  myDiv.innerHTML += domString;
+  myDiv.innerHTML = domString;
 }
 
 //merch page 
 var products = [
   { name:"Socks",
     size: ['S', 'M', 'L', 'XL' ],
-    description: "Keep your feet away from mosquito bites",
+    description: "Keep your feet away from mosquito bites!",
     imageURL:"https://image.spreadshirtmedia.com/image-server/v1/mp/products/P18092423MPC25332326/views/1,width=378,height=378,appearanceId=1,backgroundColor=E8E8E8,version=1497265829/buzz-mosquito-me",
-    price: "$2.99" ,
-    float: true,
+    price: "$3.99" ,
+   
   },
 
   { name:"Hoodie",
   size: ['S', 'M', 'L', 'XL'],
-  description: "cover your head to avoid mosquito bite",
+  description: "Cover your head with a mosquito hoodie to avoid mosquito bite!",
   imageURL:"http://www.bilgihocam.com/wp-content/uploads/2015/12/kedi-resimleri-17.jpg",
-  price: "$7.99" ,
+  price: "$8.99" ,
 },
 
 { name:"Tshirt",
   size: ['S', 'M', 'L', 'XL'],
-  description: "show off your buzz with our buzz Tshirt",
+  description: "Show off your buzz with our buzz Tshirt!",
   imageURL:"http://www.bilgihocam.com/wp-content/uploads/2015/12/kedi-resimleri-17.jpg",
-  price: "$5.99" ,
-  float: true,
+  price: "$6.99" ,
 },
 
 { name:"Hat",
   size: ['S', 'M', 'L', 'XL'],
-  description: "TLike the look of our hat?",
+  description: "Rock and Roll with our Mosquito Hats On!",
   imageURL:"http://www.bilgihocam.com/wp-content/uploads/2015/12/kedi-resimleri-17.jpg",
   price: "$4.99" ,
 }
@@ -38,40 +37,21 @@ var products = [
 
 
 function createMerchCard(){
+  var merchProject ="";
   for (var p = 0; p < products.length; p++) {
-      //  var merchProject ="";
-      // //    if( products[p].float ==true) 
-      // // {
-      // //     merchProject += "<div class = 'display-card float-right'>";
-      // // } else {
-      // //     merchProject += "<div class = 'display-card float-left'>";
-      // // }
-  
-      var merchProject ="<div class = 'dispaly-card'>";
-  
+     merchProject += "<div class = 'display-card'>"
       merchProject += "<img  class ='img-merch' src= " + products[p].imageURL + ">";
       merchProject  += "<div class='merchCard'>";
-      if( products[p].float ==true) 
-      {
-          merchProject += "<div class = 'float-right'>";
-      } else {
-          merchProject += "<div class = 'float-left'>";
-      }
       merchProject += "<h1>" + products[p].name + "</h1>";
-      
-
       merchProject += "<h3>" + products[p].size.join(' ')+ "</h3>";
-  
-
       merchProject += "<p>" + products[p].description + "</p>";
+      merchProject += "<h3>" + products[p].price + "</p>";
       merchProject += "</div>";
-      
       merchProject += "</div>";
-      writeToDom(merchProject, 'merch-page');
+      writeToDom(merchProject, "merch-page");
   }  
 
 }
-
 createMerchCard();
 
 
